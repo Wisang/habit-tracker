59016fdeb27bc5c59da9d3301fa0a05a8b19fc7a
wisang <wseom7@gmail.com>
2022-02-17 18:03:21 +0900

    graph created

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
+from wsgiref import headers
+
+import PRODUCTIVITY as PRODUCTIVITY
 import requests
 
+USER_NAME = "wisang"
+PIXELA_TOKEN = "faljqjowiflsawpqfas;lfk"
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "faljqjowiflsawpqfas;lfk",
-    "username": "wisang",
+    "token": PIXELA_TOKEN,
+    "username": USER_NAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
+graph_endpoint = f"{PIXELA_ENDPOINT}/{USER_NAME}/graphs"
+
+graph_config = {
+    'id': "productivity",
+    "name": "work hours",
+    "unit": "hour",
+    "type": "int",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": PIXELA_TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config,
+                         headers=headers)
+
+print(response.text)
+
+PRODUCTIVITY_GRAPH = "https://pixe.la/v1/users/wisang/graphs/productivity.html"
