1f19eba41fa3a21cb7057a062427696d89684054
wisang <wseom7@gmail.com>
2022-02-17 17:36:03 +0900

    account created

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "faljqjowiflsawpqfas;lfk",
+    "username": "wisang",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
