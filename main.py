6efa68573e5a5043efe739e8d72441c925f26d36
wisang <wseom7@gmail.com>
2022-02-19 21:29:45 +0900

    completed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from wsgiref import headers
-
-import PRODUCTIVITY as PRODUCTIVITY
 import requests
+
+from datetime import datetime
 
 USER_NAME = "wisang"
 PIXELA_TOKEN = "faljqjowiflsawpqfas;lfk"
@@ -14,8 +13,6 @@ user_params = {
     "notMinor": "yes"
 }
 
-graph_endpoint = f"{PIXELA_ENDPOINT}/{USER_NAME}/graphs"
-
 graph_config = {
     'id': "productivity",
     "name": "work hours",
@@ -24,13 +21,36 @@ graph_config = {
     "color": "ajisai",
 }
 
+today = datetime.now().strftime("%Y%m%d")
+
+print(today)
+
+post_pixel_config = {
+    "date": today,
+    "quantity": "3",
+}
+
+# update_pixel_config = {
+#     "quantity": "4"
+# }
+
+graph_endpoint = f"{PIXELA_ENDPOINT}/{USER_NAME}/graphs"
+post_pixel_endpoint = f"{graph_endpoint}/{graph_config['id']}"
+update_pixel_endpoint = f"{post_pixel_endpoint}/{today}"
+
 headers = {
     "X-USER-TOKEN": PIXELA_TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config,
-                         headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config,
+#                          headers=headers)
+#
+# print(response.text)
 
+response = requests.post(url=post_pixel_endpoint, headers=headers, json=post_pixel_config)
 print(response.text)
 
-PRODUCTIVITY_GRAPH = "https://pixe.la/v1/users/wisang/graphs/productivity.html"
+# response = requests.put(url=update_pixel_endpoint, headers=headers, json=update_pixel_config)
+# print(response)
+
+# PRODUCTIVITY_GRAPH = "https://pixe.la/v1/users/wisang/graphs/productivity.html"
